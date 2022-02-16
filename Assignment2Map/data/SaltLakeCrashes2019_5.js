f61fff36461d2f646009740ee89b4ad4e59bb6d3
virgi41 <97544065+virgi41@users.noreply.github.com>
2022-02-15 21:09:22 -0700

    Add files via upload

---
+++ Assignment2Map/data/SaltLakeCrashes2019_5.js
@@ -0,0 +1,24 @@
+var json_SaltLakeCrashes2019_5 = {
+"type": "FeatureCollection",
+"name": "SaltLakeCrashes2019_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "154048", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.888646, 40.642858009999827 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "154665", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -112.0244765, 40.633447199999836 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "156507", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.9408219, 40.716978039999844 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "156747", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.9397, 40.767152009999826 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "157387", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.863334, 40.523916999999827 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "157500", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -112.1051608, 40.711005479999834 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "158191", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.8016683, 40.56884542999984 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "159230", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.929498, 40.664215999999847 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "159294", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.90297, 40.640707199999845 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "160961", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.896208, 40.558945599999817 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "161745", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.8953647, 40.699802859999828 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "161875", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.891125, 40.699782249999835 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "197229", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.873643, 40.525018999999851 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "212605", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.8546157, 40.640452539999835 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "213522", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.863419, 40.641309999999834 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "246240", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.841907, 40.64021099999983 ] ] } },
+{ "type": "Feature", "properties": { "YEAR": "2019", "objectid": "263799", "pedestrian": "true" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -111.93663, 40.644366999999846 ] ] } }
+]
+}
